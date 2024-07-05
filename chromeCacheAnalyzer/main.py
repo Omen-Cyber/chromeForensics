fe8d278c16bf4523b0c27f4f39ba259f76dd9ee0
ak <fkrepelka@gmail.com>
2024-07-06 02:34:16 +0300

    Creating real main, creating base cacheExtractor class

--- chromeCacheAnalyzer/main.py
+++ chromeCacheAnalyzer/main.py
@@ -1,8 +1,21 @@
-import utils chromeCacheExtractor
+from chromeCacheExtractor import cacheExtractor
+import argparse
 
-if __name__ == '__main__':
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Analyzes directory of Chrome cache files")
 
-    if len(sys.argv) < 3:
-        print(f"USAGE: {pathlib.Path(sys.argv[0]).name} <cache input dir> <out dir>")
-        exit(1)
-    main(sys.argv[1:])
+    # Add arguments
+    parser.add_argument('-c', type=str, required=True, help='Path to the Chromium cache directory')
+    parser.add_argument('-d', type=str, required=True, help='Path to the output directory')
+    parser.add_argument('-o', choices=['json', 'csv'], required=True, help='Output format: json or csv')
+
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    c = cacheExtractor(args.c, args.d, args.o)
+    c.parse_cache_entries()
